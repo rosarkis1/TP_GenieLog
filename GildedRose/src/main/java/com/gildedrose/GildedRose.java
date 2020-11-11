8483853689704d117c6de8bac07119285db6462f
rosarkis1 <71924855+rosarkis1@users.noreply.github.com>
2020-11-11 20:44:09 +0100

    v2.1 Refactoring

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -9,12 +9,12 @@ class GildedRose {
 
     public void updateQuality() {
         for(Item item : items) {
-            if(item.name.equals("Aged Brie")){
-                IncrementItemQuality(item);
-            }
-            else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                if(item.quality<50){
-                    item.quality+=1;
+            switch (item.name) {
+                case "Aged Brie":
+                    IncrementItemQuality(item);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    IncrementItemQuality(item);
 
                     if(item.sellIn<11){
                         IncrementItemQuality(item);
@@ -23,14 +23,13 @@ class GildedRose {
                     if(item.sellIn<6){
                         IncrementItemQuality(item);
                     }
-                }
-            }
 
-            else {
-
-                if(!item.name.equals("Sulfuras, Hand of Ragnaros")){
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    break;
+                default:
                     DecrementItemQuality(item);
-                }
+                    break;
             }
 
             if(item.name.equals("Sulfuras, Hand of Ragnaros")){
@@ -38,20 +37,22 @@ class GildedRose {
                 item.sellIn-=1;
             }
 
-            if(item.sellIn<0){
-                if(item.name.equals("Aged Brie")){
-                    IncrementItemQuality(item);
-                }else {
-                    if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                        item.quality=0;
-                    }else {
 
-                        if(item.name.equals("Sulfuras, Hand of Ragnaros")){
-                            continue;
-                        }
+            switch (item.name) {
+                case "Sulfuras, Hand of Ragnaros":
+                    continue;
+                case "Aged Brie":
+                    if(item.sellIn<0)
+                        IncrementItemQuality(item);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if(item.sellIn<0)
+                        item.quality=0;
+                    break;
+                default:
+                    if(item.sellIn<0)
                         DecrementItemQuality(item);
-                    }
-                }
+                    break;
             }
         }
     }
